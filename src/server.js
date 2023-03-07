0080f5eda5c1ecc80a63f384065c4809979f9058
jacoobanderson <j00cwa@gmail.com>
2023-03-07 20:54:36 +0100

    Create initial server.js structure

---
+++ src/server.js
@@ -0,0 +1,70 @@
+import express from 'express'
+import helmet from 'helmet'
+import logger from 'morgan'
+import createError from 'http-errors'
+import { router } from './routes/router.js'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import expressLayouts from 'express-ejs-layouts'
+
+try {
+  const app = express()
+
+  const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+  // Set various HTTP headers to make the application little more secure (https://www.npmjs.com/package/helmet).
+  app.use(helmet())
+
+  // Set up a morgan logger using the dev format for log entries.
+  app.use(logger('dev'))
+
+  // View engine set up.
+  app.set('view engine', 'ejs')
+  app.set('views', join(directoryFullName, 'views'))
+  app.use(expressLayouts)
+  app.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
+
+  // Parse requests of the content type application/json.
+  app.use(express.json())
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    if (!err.status) {
+      const cause = err
+      err = createError(500)
+      err.cause = cause
+    }
+
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        cause: err.cause ? JSON.stringify(err.cause, Object.getOwnPropertyNames(err.cause)) : undefined,
+        stack: err.stack
+      })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
