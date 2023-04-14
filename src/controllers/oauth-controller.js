baca13453e7d070c520d2ad690831d91ffbf00ea
jacoobanderson <j00cwa@gmail.com>
2023-04-14 19:23:25 +0200

    Fix gitlab redirect to profile

--- src/controllers/oauth-controller.js
+++ src/controllers/oauth-controller.js
@@ -12,6 +12,12 @@ export default class OauthController {
    * @param {Function} next Express next function
    */
   async handleCallback (req, res, next) {
+    try {
+        console.log('tesetsetsetsetsetstststsset')
+    } catch (error) {
+        next(error)
+    }
+    next()
   }
 
   /**
@@ -22,17 +28,23 @@ export default class OauthController {
    * @param {Function} next Express next function
    */
   async getUser (req, res, next) {
-
+    try {
+        console.log('UUUUSER')
+    } catch (error) {
+        next(error)
+    }
+    next()
   }
 
   /**
-   * Gets the user information.
+   * Redirects the user to the profile.
    *
    * @param {object} req  Express request object
    * @param {object} res Express response object
    * @param {Function} next Express next function
    */
   async redirectToProfile (req, res, next) {
-
+    console.log('REEEEEENDER')
+    res.redirect('/user/profile')
   }
 }
